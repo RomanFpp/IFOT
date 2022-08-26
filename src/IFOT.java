16690c3decb891738e7f783508ce9ea0939b36c2
Roman <pomrfv@gmail.com>
2022-08-26 17:35:50 +0200

    True-False checking

--- src/IFOT.java
+++ src/IFOT.java
@@ -1,13 +1,13 @@
 public class IFOT {
     public static void main(String[] args) {
         int x = 0;
-        boolean a = true;
+        boolean a = false;
 
         while (!a) {
             System.out.println(x);
             x++;
             if (x == 10) {
-                a = false;
+                a = true;
             }
         }
     }
